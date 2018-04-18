b806b26d256363d1fea69ccd5734140a142b6594
whynjust <hw1456@nyu.edu>
2018-04-18 17:48:36 -0400

    Setup start script and heroku port

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const hbs = require('hbs');
 
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -29,9 +31,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -77,4 +79,6 @@ app.get('/about', (req, res) => {
     });
 })
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`connected ${port}`);
+});
